defc621e0ebfd4f56b0308378626e40f243aae1a
Bruno Turcksin <bruno.turcksin@gmail.com>
2019-07-01 20:00:51 +0000

    Add CUDA matrix-free hierarchy helpers

---
+++ include/mfmg/cuda/cuda_matrix_free_hierarchy_helpers.cuh
@@ -0,0 +1,45 @@
+/*************************************************************************
+ * Copyright (c) 2017-2019 by the mfmg authors                           *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * This file is part of the mfmg libary. mfmg is distributed under a BSD *
+ * 3-clause license. For the licensing terms see the LICENSE file in the *
+ * top-level directory                                                   *
+ *                                                                       *
+ * SPDX-License-Identifier: BSD-3-Clause                                 *
+ *************************************************************************/
+
+#ifndef MFMG_CUDA_MATRIX_FREE_HIERARCHY_HELPERS_CUH
+#define MFMG_CUDA_MATRIX_FREE_HIERARCHY_HELPERS_CUH
+
+#ifdef MFMG_WITH_CUDA
+
+#include <mfmg/cuda/cuda_hierarchy_helpers.cuh>
+
+namespace mfmg
+{
+template <int dim, typename VectorType>
+class CudaMatrixFreeHierarchyHelpers final
+    : public CudaHierarchyHelpers<dim, VectorType>
+{
+public:
+  using vector_type = VectorType;
+
+  CudaMatrixFreeHierarchyHelpers(CudaHandle const &cuda_handle);
+
+  virtual std::shared_ptr<Operator<VectorType>>
+  get_global_operator(std::shared_ptr<MeshEvaluator> mesh_evaluator) override;
+
+  virtual std::shared_ptr<Operator<VectorType>> build_restrictor(
+      MPI_Comm comm, std::shared_ptr<MeshEvaluator> mesh_evaluator,
+      std::shared_ptr<boost::property_tree::ptree const> params) override;
+
+  virtual std::shared_ptr<Smoother<VectorType>> build_smoother(
+      std::shared_ptr<Operator<VectorType> const> op,
+      std::shared_ptr<boost::property_tree::ptree const> params) override;
+};
+} // namespace mfmg
+
+#endif
+
+#endif
